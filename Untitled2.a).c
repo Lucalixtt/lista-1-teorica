b1e9806d2a18561944e78bbff8dc76c04a0584d6
Lucalixtt <luizacturatti@gmail.com>
2024-09-09 11:04:34 -0300

    Add files via upload

---
+++ Untitled2.a).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+    int a;
+    int b;
+    int c;
+
+    printf("digite 3 números inteiros:\n");
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+
+    printf("\n%d\n",c);
+    printf("%d\n",b);
+    printf("%d\n",a);
+
+return 0;
+}
